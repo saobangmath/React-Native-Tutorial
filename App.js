f4751cc6c0633bf41da4ec0542916e99b548b2c4
saobangmath <anhtai2672000@gmail.com>
2020-05-26 08:43:21 +0800

    First commit

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, ScrollView, TextInput ,Image} from 'react-native';
+import {SearchBar} from 'react-native-elements';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Korotkevich_ITMO.jpg'}}style={{flex:1}} />
+      <View style={{flexDirection:'column', flex:1}}>
+          <Text style={styles.text}>Hi I am Tran Anh Tai!</Text>
+          <Text style={styles.text}>Hi I am Tran Anh Tai!</Text>
+          <Text style={styles.text}>Hi I am Tran Anh Tai!</Text>
+          <Text style={styles.text}>Hi I am Tran Anh Tai!</Text>
+          <Text style={styles.text}>Hi I am Tran Anh Tai!</Text>
+          <Text style={styles.text}>Hi I am Tran Anh Tai!</Text>
+          <SearchBar
+          placeholder="Type Here ..."
+          onChangeText={console.log("HELLO!")}>
+
+          </SearchBar>
+      </View>
     </View>
   );
 }
@@ -13,7 +26,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    //alignItems: 'center',
+    flexDirection: 'column',  
     justifyContent: 'center',
   },
+  text: {
+    backgroundColor: '#0f0',
+    alignItems: 'center', 
+    alignSelf:'center',
+
+  }
 });
