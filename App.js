f0741b75af304a323abbef37a55568256f56d6ee
saobangmath <anhtai2672000@gmail.com>
2020-05-28 17:13:24 +0800

    Add list of big companies

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react'; 
 import { StyleSheet, Text, TextInput, View, ScrollView ,Image, FlatList, Button, SectionList} from 'react-native'; // components;
 import {Dimensions} from 'react-native';
+import {NavigationContainer, StackActions} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
 /**
  * constant variable define;
  */
-const image = {uri:'https://upload.wikimedia.org/wikipedia/commons/4/4c/Korotkevich_ITMO.jpg', width:400, height:400, flex:1};
+const image = {uri:'https://upload.wikimedia.org/wikipedia/commons/4/4c/Korotkevich_ITMO.jpg', 
+               method: "GET", 
+               width:400, height:400, flex:1};
+
 const screen = Dimensions.get('window');
+
 const achievements = [{key:"Google CodeJam champion from 2014-2019"}, 
                       {key:"IOI gold medalist from 2007-2012"}, 
                       {key:"ACM-ICPC World Champion in 2013 and 2015"}, 
@@ -20,8 +27,30 @@ const winning_match = [{year:2014, data:["Google Code Jam", "Facebook Hacker Cup
                        {year:2018, data:["Google Code Jam", "Google HashCode Champion"]},
                        {year:2019, data:["Google Code Jam", "Facebook Hacker Cup"]},
                       ];
-
+const organizations = [ {name : "Google",   uri: require("./images/Google.png")},
+                        {name : "Facebook", uri: require("./images/Facebook.png")},
+                        {name : "Yandex",   uri: require("./images/Yandex.png")},
+                        {name : "Hackerank",uri: require("./images/Hackerank.png")}   
+                      ];
+const Stack = createStackNavigator();
 var msg = "";
+/**
+ *  My stack function
+ */
+function MyStack(){
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name = "Home"
+          component={Home}
+          options={{title:"Welcome"}}
+        />
+        <Stack.Screen name = "Profile" component = {Profile}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
 /**
  * main app module;
  */
@@ -34,10 +63,11 @@ export default function App() {
       <ScrollView style={{flexDirection:'column', flex:1}}>
         <View>
             <Text style={styles.section}>Some "decent" achievements:</Text>
-             {/* flat list component; */}
-              <FlatList data={achievements} renderItem={({item})=>
-                 <Text style={styles.text}>{item.key}</Text>}/>
-              {/* section list component; */}
+             {/* flat list of great achievements  */}
+              <FlatList data={achievements} 
+                        renderItem={({item})=> 
+                        <Text style={styles.text}>{item.key}</Text>}/>
+              {/* section list of winning matches; */}
               <SectionList
                 sections={winning_match}
                 renderItem={({item})=>
@@ -45,20 +75,25 @@ export default function App() {
                 }
                 
                 renderSectionHeader={({section}) => 
-                <Text style={styles.section}>{section.year}</Text>}
-                keyExtractor={(item, match)=> match}
+                  <Text style={styles.section}>{section.year}</Text>}
+                  keyExtractor={(item, match)=> match
+                }
               />
         </View>
+        <View>
+          <Text style={styles.section}>Big tech companies related</Text>
+          {/* flatlist of all organization have participated for competitions*/}
+              <FlatList data={organizations}
+                    horizontal = {true}
+                    renderItem = {({item}) => 
+              <View style = {styles.container}>
+                <Text style = {styles.text}>{item.name}</Text> 
+                <Image source = {item.uri}
+                    style = {styles.card}></Image>
+              </View>
+            }/>
+        </View>
       </ScrollView>
-
-      <View style={styles.button}>
-        <Button 
-                onPress={() =>{
-                  alert(msg);
-                }}
-                title="PRESS ME"
-        />
-      </View>
     </View>
   );
 }
@@ -93,18 +128,18 @@ const styles = StyleSheet.create({
     justifyContent:'space-between',
     padding:10,     
     alignItems: 'center', 
-    alignSelf:'center'
+    alignSelf:'center', 
+    marginLeft:10
   },
   textInput: {
     alignSelf:'flex-start',
     fontSize: 16,
   },
-  button:{
-    alignSelf:"center",
-    bottom: 10,
-    backgroundColor:'#00aeef', 
-    width: screen.width / 3,
-    fontWeight: "bold"
+  card: {
+    flex : 1,
+    alignSelf: 'center',
+    width : 40,
+    height: 40,
   }
 });
 
