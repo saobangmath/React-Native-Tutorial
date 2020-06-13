4f5749f8b6948ef018cee8a9eb028038899c2e9e
saobangmath <anhtai2672000@gmail.com>
2020-06-13 11:38:23 +0800

    React Hook added for managing state of input

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
-import React from 'react'; 
-import { StyleSheet, Text, View, ScrollView ,Image, FlatList, SectionList} from 'react-native'; // components;
+import React, { useState} from 'react'; 
+import {Keyboard, StyleSheet, Text, View, ScrollView ,Image, TextInput, FlatList, SectionList} from 'react-native'; // components;
 import {NavigationContainer, StackActions} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {image, screen, achievements, winning_match, organizations}  from './Constants'
@@ -8,11 +8,27 @@ import {image, screen, achievements, winning_match, organizations}  from './Cons
  * main app module;
  */
 export default function App() {
+  // inputState is an array with 2 element:
+  // inputState[0]: the current state of the 
+  const inputState = useState({title : '', amount : ''});
   return (
     <View style={styles.container}>
       <Image source={image}/>
       <Text style={styles.header}>Gennady Korotkevich</Text>
-      
+      <TextInput type = "text" 
+                 style = {styles.input}
+                 placeholder = "title"
+                 value ={inputState[0].title} 
+                 onChange = {event => inputState[1]({title : event.target.value})}
+                 onTouchMove = {() => Keyboard.dismiss()}
+                 />
+      <TextInput type = "text"
+                 style = {styles.input}
+                 placeholder = "amount"
+                 value = {inputState[0].title}
+                 onChange = {event => inputState[1]({amount : event.target.value})}
+                 onTouchMove = {() => Keyboard.dismiss()}
+                 />
       <ScrollView style={{flexDirection:'column', flex:1}}>
         <View>
             <Text style={styles.section}>Some "decent" achievements:</Text>
@@ -50,49 +66,48 @@ export default function App() {
     </View>
   );
 }
-/**
- * rules to define the components styles: from GENERAL to DETAILS
- * position -> color -> size -> 
- */
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    flexDirection: 'column',  
-    justifyContent: 'center',
+  input: {
+    textAlign: "center"
   },
-  header:{
-    color: "red",
-    alignSelf: "center",
-    fontStyle: "italic",
-    fontWeight: "bold", 
-    fontSize:30
+  container:{
+      flex: 1,
+      backgroundColor: '#fff',
+      flexDirection: 'column',  
+      justifyContent: 'center'
+  },
+  header: {
+      color: "red",
+      alignSelf: "center",
+      fontStyle: "italic",
+      fontWeight: "bold",
+      fontSize:30,
   },
   section:{
-    width:screen.width,
-    backgroundColor: "#0f0",
-    fontSize:20, 
-    fontWeight:"bold",
-    textAlign:"center"
-  }, 
-  text: {
-    color: '#00f',
-    fontSize:16,
-    justifyContent:'space-between',
-    padding:10,     
-    alignItems: 'center', 
-    alignSelf:'center', 
-    marginLeft:10
+      width:"100%",
+      backgroundColor: "#0f0",
+      fontSize:20,
+      fontWeight:"bold",
+      textAlign:"center",
+  },
+  text:{
+      color: '#00f',
+      fontSize:16,
+      justifyContent:'space-between',
+      padding:10,    
+      alignItems: 'center', 
+      alignSelf:'center',
+      marginLeft:10,
   },
   textInput: {
-    alignSelf:'flex-start',
-    fontSize: 16,
+      alignSelf:'flex-start',
+      fontSize: 16,
   },
-  card: {
-    flex : 1,
-    alignSelf: 'center',
-    width : 40,
-    height: 40,
+  card:{
+      flex : 1,
+      alignSelf: 'center',
+      width : 40,
+      height: 40
   }
 });
-
